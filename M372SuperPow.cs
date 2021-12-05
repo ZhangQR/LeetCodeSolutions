6ba06627cb34a3b8d48f247fa756c4493b107862
ZhangQR <ricey54560@gmail.com>
2021-12-05 22:41:54 +0800

    m372

---
+++ M372SuperPow.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCodeSolutions
+{
+    /// <summary>
+    /// 笨蛋解法，自然超时了
+    /// </summary>
+    public class M372SuperPow
+    {
+        public int SuperPow(int a, int[] b)
+        {
+            bool isZero = true;
+            long crtResult = 1;
+            int bLength;
+            while (true)
+            {
+                isZero = true;
+                foreach (var num in b)
+                {
+                    if (num != 0)
+                    {
+                        isZero = false;
+                        break;
+                    }
+                }
+                if (isZero)
+                {
+                    return (int)crtResult;
+                }
+                
+                bLength = b.Length;
+                if (b[bLength-1] > 0)
+                {
+                    b[bLength - 1]--;
+                }
+                else
+                {
+                    b[bLength - 1] = 9;
+                    for (int i = bLength - 2; i >= 0; i--)
+                    {
+                        if (b[i] > 0)
+                        {
+                            b[i]--;
+                            break;
+                        }
+                        b[i] = 9;
+                    }
+                }
+                crtResult = a * crtResult % 1337;
+            }
+            return (int)crtResult;
+        }
+    }
+}
---
+++ M372SuperPow2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class M372SuperPow2
+    {
+        private const int Mod = 1337;
+        public int SuperPow(int a, int[] b)
+        {
+            a %= Mod;
+            int length = b.Length;
+            if (b.Length == 1)
+            {
+                return MyPow(a, b[0]) % Mod;
+            }
+            List<int> nextB = new List<int>(b);
+            nextB.RemoveAt(length-1);
+            return MyPow(a, b[length-1]) * MyPow(SuperPow(a, nextB.ToArray()), 10) % Mod;
+        }
+
+        private int MyPow(int x, int y)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            return x * MyPow(x, --y) % Mod;
+        }
+    }
+}
